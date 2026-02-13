3c9131d066ed9fc439dca3c51115c76bbc52c33d
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2026-02-13 14:20:46 -0400

    feat: program

---
+++ bin/backup.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const { program } = require("commander");
+const { runBackup } = require("../lib/runner");
+const { addPath, listPaths, removePath, setDestination, setSchedule } = require("../lib/config");
+const { startScheduler } = require("../lib/scheduler");
+
+program
+  .command("run")
+  .description("Executar backup manualmente")
+  .action(runBackup);
+
+program
+  .command("add <path>")
+  .description("Adicionar novo caminho para backup")
+  .action(addPath);
+
+program
+  .command("remove <path>")
+  .description("Remover caminho do backup")
+  .action(removePath);
+
+program
+  .command("list")
+  .description("Listar caminhos configurados")
+  .action(listPaths);
+
+program
+  .command("dest <path>")
+  .description("Definir destino base do backup")
+  .action(setDestination);
+
+program
+  .command("schedule <cron>")
+  .description("Definir agendamento (formato cron)")
+  .action(setSchedule);
+
+program
+  .command("start")
+  .description("Iniciar agendador")
+  .action(startScheduler);
+
+program
+  .command("interval <days>")
+  .description("Executar backup a cada X dias")
+  .action((days) => {
+    require("../lib/scheduler").startInterval(parseInt(days));
+  });
+
+program.parse();
